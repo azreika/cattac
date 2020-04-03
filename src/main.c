68d6bd2679cd293c8f0c7a37d3b0f5837313d81f
Abdul Zreika <azre6702@uni.sydney.edu.au>
2020-04-03 21:16:36 +1100

    Initialised repository.

---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    printf("Welcome to CattaC.\n");
+    return 0;
+}
---
+++ test/test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    printf("Running tests...\n");
+    printf("Tests complete.\n");
+}
