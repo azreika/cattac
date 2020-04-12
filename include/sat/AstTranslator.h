a4147847473254bc6e0b1b8ae4a10a8747e9ffb0
Abdul Zreika <azre6702@uni.sydney.edu.au>
2020-04-12 21:12:39 +1000

    Cleaned up AstTranslator class.

--- include/sat/AstTranslator.h
+++ include/sat/AstTranslator.h
@@ -1,28 +1,77 @@
 #pragma once
 
 #include <map>
+#include <string>
 
 #include "AstNode.h"
 #include "SatNode.h"
 
+/**
+ * Translation unit that converts an AST formula into a more restricted SAT formula.
+ */
 class AstTranslator {
 public:
     AstTranslator(const AstNode* formula) : formula(formula) {
         run();
     }
 
+    /**
+     * Return the SAT formula represented by the input AST.
+     */
     SatConjunction* getSatFormula() const {
         return result;
     }
+
 private:
+    /**
+     * Types of internal assignments possible.
+     */
+    enum class AssignmentType {
+        AND, NOT, OR,
+    };
+
+    /**
+     * Represents an internal assignment.
+     */
+    struct Assignment {
+        AssignmentType type;
+        std::string name;
+        std::string op1;
+        std::string op2;
+    };
+
     const AstNode* formula;
     std::map<std::string, AstNode*> subformulaNames{};
     size_t numVars{0};
+    std::vector<Assignment> assignments{};
     SatConjunction* result{new SatConjunction()};
 
+    /**
+     * Run the translator.
+     */
     void run();
 
+    /**
+     * Add an assignment to the internal correspondence list.
+     * @param type operand type of assignment to construct
+     * @param name name of the new variable created
+     * @param left operand of the assignment
+     * @param right operand of the assignment (optional)
+     */
+    void addAssignment(AssignmentType type, std::string name, std::string operand1, std::string operand2 = "") {
+        assignments.push_back({type, name, operand1, operand2});
+    }
+
+    /**
+     * Assign a name to every subformula in the given node.
+     * @param node node to assign a name to
+     * @return name assigned to the subformula
+     */
     std::string nameSubformulas(const AstNode* node);
 
+    /**
+     * Construct a new unique name.
+     * @return a name that has not previously been generated
+     */
     std::string nextName();
 };
--- src/sat/AstTranslator.cpp
+++ src/sat/AstTranslator.cpp
@@ -4,86 +4,90 @@
 #include "AstTranslator.h"
 
 void AstTranslator::run() {
-    nameSubformulas(formula);
-    for (const auto& pair : subformulaNames) {
-        const std::string& name = pair.first;
-        const AstNode* sub = pair.second;
-        if (const AstAnd* andOp = dynamic_cast<const AstAnd*>(sub)) {
-            std::string A = static_cast<AstVar*>(andOp->getLeft())->getName();
-            std::string B = static_cast<AstVar*>(andOp->getRight())->getName();
+    // Construct the first disjunction - assigns "true" to the full formula
+    std::string fullFormulaVariable = nameSubformulas(formula);
+    SatDisjunction* formulaTruth = new SatDisjunction();
+    formulaTruth->addAtom(new SatAtom(fullFormulaVariable, false));
+    result->addDisjunction(formulaTruth);
 
-            SatDisjunction* first = new SatDisjunction();
-            first->addAtom(new SatAtom(A, true));
-            first->addAtom(new SatAtom(B, true));
-            first->addAtom(new SatAtom(name, false));
-            result->addDisjunction(first);
+    // Construct disjunctions to model all variable assignments created
+    for (const auto& assignment : assignments) {
+        switch (assignment.type) {
+            case AssignmentType::AND: {
+                SatDisjunction* first = new SatDisjunction();
+                first->addAtom(new SatAtom(assignment.op1, true));
+                first->addAtom(new SatAtom(assignment.op2, true));
+                first->addAtom(new SatAtom(assignment.name, false));
+                result->addDisjunction(first);
 
-            SatDisjunction* second = new SatDisjunction();
-            second->addAtom(new SatAtom(A, false));
-            second->addAtom(new SatAtom(name, true));
-            result->addDisjunction(second);
+                SatDisjunction* second = new SatDisjunction();
+                second->addAtom(new SatAtom(assignment.op1, false));
+                second->addAtom(new SatAtom(assignment.name, true));
+                result->addDisjunction(second);
 
-            SatDisjunction* third = new SatDisjunction();
-            third->addAtom(new SatAtom(B, false));
-            third->addAtom(new SatAtom(name, true));
-            result->addDisjunction(third);
-        } else if (const AstOr* orOp = dynamic_cast<const AstOr*>(sub)) {
-            std::string A = static_cast<AstVar*>(orOp->getLeft())->getName();
-            std::string B = static_cast<AstVar*>(orOp->getRight())->getName();
+                SatDisjunction* third = new SatDisjunction();
+                third->addAtom(new SatAtom(assignment.op2, false));
+                third->addAtom(new SatAtom(assignment.name, true));
+                result->addDisjunction(third);
+                break;
+            }
+            case AssignmentType::OR: {
+                SatDisjunction* first = new SatDisjunction();
+                first->addAtom(new SatAtom(assignment.op1, false));
+                first->addAtom(new SatAtom(assignment.op2, false));
+                first->addAtom(new SatAtom(assignment.name, true));
+                result->addDisjunction(first);
 
-            SatDisjunction* first = new SatDisjunction();
-            first->addAtom(new SatAtom(A, false));
-            first->addAtom(new SatAtom(B, false));
-            first->addAtom(new SatAtom(name, true));
-            result->addDisjunction(first);
+                SatDisjunction* second = new SatDisjunction();
+                second->addAtom(new SatAtom(assignment.op1, true));
+                second->addAtom(new SatAtom(assignment.name, false));
+                result->addDisjunction(second);
 
-            SatDisjunction* second = new SatDisjunction();
-            second->addAtom(new SatAtom(A, true));
-            second->addAtom(new SatAtom(name, false));
-            result->addDisjunction(second);
+                SatDisjunction* third = new SatDisjunction();
+                third->addAtom(new SatAtom(assignment.op2, true));
+                third->addAtom(new SatAtom(assignment.name, false));
+                result->addDisjunction(third);
+                break;
+            }
+            case AssignmentType::NOT: {
+                assert(assignment.op2 == "" && "unexpected second operand");
 
-            SatDisjunction* third = new SatDisjunction();
-            third->addAtom(new SatAtom(B, true));
-            third->addAtom(new SatAtom(name, false));
-            result->addDisjunction(third);
-        } else if (const AstNot* notOp = dynamic_cast<const AstNot*>(sub)) {
-            std::string A = static_cast<AstVar*>(notOp->getOperand())->getName();
+                SatDisjunction* first = new SatDisjunction();
+                first->addAtom(new SatAtom(assignment.op1, true));
+                first->addAtom(new SatAtom(assignment.name, true));
+                result->addDisjunction(first);
 
-            SatDisjunction* first = new SatDisjunction();
-            first->addAtom(new SatAtom(A, true));
-            first->addAtom(new SatAtom(name, true));
-            result->addDisjunction(first);
-
-            SatDisjunction* second = new SatDisjunction();
-            second->addAtom(new SatAtom(A, false));
-            second->addAtom(new SatAtom(name, false));
-            result->addDisjunction(second);
-        } else {
-            assert(dynamic_cast<const AstVar*>(sub) && "unexpected ast node type");
+                SatDisjunction* second = new SatDisjunction();
+                second->addAtom(new SatAtom(assignment.op1, false));
+                second->addAtom(new SatAtom(assignment.name, false));
+                result->addDisjunction(second);
+                break;
+            }
+            default:
+                assert(false && "unexpected assignment type");
         }
     }
 }
 
 std::string AstTranslator::nameSubformulas(const AstNode* node) {
     if (const AstAnd* andOp = dynamic_cast<const AstAnd*>(node)) {
-        const std::string& left = nameSubformulas(andOp->getLeft());
-        const std::string& right = nameSubformulas(andOp->getRight());
+        std::string left = nameSubformulas(andOp->getLeft());
+        std::string right = nameSubformulas(andOp->getRight());
         std::string newName = nextName();
-        subformulaNames[newName] = new AstAnd(new AstVar(left), new AstVar(right));
+        addAssignment(AssignmentType::AND, newName, left, right);
         return newName;
     } else if (const AstOr* orOp = dynamic_cast<const AstOr*>(node)) {
-        const std::string& left = nameSubformulas(orOp->getLeft());
-        const std::string& right = nameSubformulas(orOp->getRight());
+        std::string left = nameSubformulas(orOp->getLeft());
+        std::string right = nameSubformulas(orOp->getRight());
         std::string newName = nextName();
-        subformulaNames[newName] = new AstOr(new AstVar(left), new AstVar(right));
+        addAssignment(AssignmentType::OR, newName, left, right);
         return newName;
     } else if (const AstNot* notOp = dynamic_cast<const AstNot*>(node)) {
-        const std::string& op = nameSubformulas(notOp->getOperand());
+        std::string op = nameSubformulas(notOp->getOperand());
         std::string newName = nextName();
-        subformulaNames[newName] = new AstNot(new AstVar(op));
+        addAssignment(AssignmentType::NOT, newName, op);
         return newName;
     } else if (const AstVar* varOp = dynamic_cast<const AstVar*>(node)) {
-        subformulaNames[varOp->getName()] = new AstVar(varOp->getName());
         return varOp->getName();
     } else {
         assert(false && "unexpected ast node type");
